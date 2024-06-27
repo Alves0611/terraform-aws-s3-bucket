b9f5a7dc26e3c342dd1962a90f5f46848ff93c8c
Alves0611 <g.alves0611@gmail.com>
2024-06-27 13:28:35 -0300

    feat: add S3 bucket ACL, policy, and versioning resources
    - Added 'aws_s3_bucket_acl' resource to set the bucket ACL
    - Added 'aws_s3_bucket_policy' resource to define the bucket policy
    - Added 'aws_s3_bucket_versioning' resource to configure versioning settings

--- main.tf
+++ main.tf
@@ -21,3 +21,37 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = lookup(var.public_access, "ignore_public_acls", true)
   restrict_public_buckets = lookup(var.public_access, "restrict_public_buckets", true)
 }
+
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.this,
+    aws_s3_bucket_public_access_block.this
+  ]
+
+  bucket = aws_s3_bucket.this.bucket
+  acl    = var.acl
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  count = var.policy != null ? 1 : 0
+
+  depends_on = [
+    aws_s3_bucket_acl.this,
+  ]
+
+  bucket = aws_s3_bucket.this.id
+  policy = var.policy.json
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  count = var.versioning.status != null ? 1 : 0
+
+  bucket                = aws_s3_bucket.this.id
+  expected_bucket_owner = lookup(var.versioning, "expected_bucket_owner", null)
+  mfa                   = lookup(var.versioning, "mfa", null)
+
+  versioning_configuration {
+    status     = lookup(var.versioning, "status", null)
+    mfa_delete = lookup(var.versioning, "mfa_delete", null)
+  }
+}
